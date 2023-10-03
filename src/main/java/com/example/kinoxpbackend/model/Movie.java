a49ddf5be38862c2d47d324a30b140052110c7d0
EmailKolar <Emilaalund.nyboder@gmail.com>
2023-10-03 11:57:31 +0200

    test push

--- src/main/java/com/example/kinoxpbackend/model/Movie.java
+++ src/main/java/com/example/kinoxpbackend/model/Movie.java
@@ -1,4 +1,32 @@
 package com.example.kinoxpbackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="movie")
 public class Movie {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long movie_id;
+
+    private String movie_name;
+//    private int total_tickets_sold;
+    private String movie_description;
+    private String genre;
+    private int age_restriction;
+    private int runtime;
+    private String trailer_link;
+    private String poster_link;
+
+
 }
