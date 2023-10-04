5a66ff7569646ee01d9f402ce1c2f4b5b30b3f39
EmailKolar <Emilaalund.nyboder@gmail.com>
2023-10-04 10:32:43 +0200

    employee auto_increment added

--- src/main/java/com/example/kinoxpbackend/model/Employee.java
+++ src/main/java/com/example/kinoxpbackend/model/Employee.java
@@ -1,10 +1,7 @@
 package com.example.kinoxpbackend.model;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +16,7 @@ import lombok.Setter;
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "employee_id")
     private Long employee_id;
 
