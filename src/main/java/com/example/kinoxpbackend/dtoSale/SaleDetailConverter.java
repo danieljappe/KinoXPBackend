d9518beb716a6e211a77339784acb663f6674cc1
cihadozdemir <ciha0128@gmail.com>
2023-10-05 09:48:28 +0200

    DTO for Saleitem Sale SaleDetail ikke færdig 2

--- src/main/java/com/example/kinoxpbackend/dtoSale/SaleDetailConverter.java
+++ src/main/java/com/example/kinoxpbackend/dtoSale/SaleDetailConverter.java
@@ -1,24 +1,12 @@
 package com.example.kinoxpbackend.dtoSale;
 
 import com.example.kinoxpbackend.model.SaleDetail;
-import com.example.kinoxpbackend.model.SaleItem;
-import com.example.kinoxpbackend.repositoryAllSale.SaleItemRepository;
-import com.example.kinoxpbackend.repositoryAllSale.SaleRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SaleDetailConverter {
 
-    private final SaleRepository saleRepository;
-    private final SaleItemRepository saleItemRepository;
-
-    public SaleDetailConverter(SaleRepository saleRepository, SaleItemRepository saleItemRepository) {
-        this.saleRepository = saleRepository;
-        this.saleItemRepository = saleItemRepository;
-    }
-
     public SaleDetailDTO toDTO(SaleDetail saleDetail) {
-
         return new SaleDetailDTO(
                 saleDetail.getSaleDetailId(),
                 saleDetail.getSale(),
@@ -28,21 +16,12 @@ public class SaleDetailConverter {
     }
 
     public SaleDetail toEntity(SaleDetailDTO saleDetailDTO) {
-
         SaleDetail saleDetail = new SaleDetail();
         saleDetail.setSaleDetailId(saleDetailDTO.saleDetailID());
-/*
-        Sale sale = saleRepository.findById(saleDetailDTO.saleId())
-                .orElseThrow(() -> new EntityNotFoundException("Sale not found for id: " + saleDetailDTO.saleId()));
-        saleDetail.setSale(sale);
-
-        SaleItem saleItem = saleItemRepository.findById(saleDetailDTO.saleItemId())
-                .orElseThrow(() -> new EntityNotFoundException("SaleItem not found for id: " + saleDetailDTO.saleItemId()));
-        saleDetail.setSaleItem(saleItem);
-
-
- */
+        saleDetail.setSale(saleDetailDTO.sale());
+        saleDetail.setSaleItem(saleDetailDTO.saleItem());
         saleDetail.setQuantity(saleDetailDTO.quantity());
+
         return saleDetail;
     }
 }
