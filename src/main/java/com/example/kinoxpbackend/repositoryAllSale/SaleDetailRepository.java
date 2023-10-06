a2972bf3ae95dca710a7b3d9bb7bb998c26f2d4d
cihadozdemir <ciha0128@gmail.com>
2023-10-06 12:07:51 +0200

    rettelser for det hele, + SaleItem og Sale post og get kan laves

--- src/main/java/com/example/kinoxpbackend/repositoryAllSale/SaleDetailRepository.java
+++ src/main/java/com/example/kinoxpbackend/repositoryAllSale/SaleDetailRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SaleDetailRepository extends JpaRepository<SaleDetail, Integer> {
+public interface SaleDetailRepository extends JpaRepository<SaleDetail, Long> {
 }
