4cdbb9afd57b72ae120dcd06acb565892defdcaf
MatDat <math03t7@stud.kea.dk>
2023-10-24 13:31:53 +0200

    Test

--- src/main/java/com/example/kinoxpbackend/model/Customer.java
+++ src/main/java/com/example/kinoxpbackend/model/Customer.java
@@ -1,6 +1,5 @@
 package com.example.kinoxpbackend.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
